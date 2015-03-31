8dc7414eae07d12527c37f8eaec0dc47dce8dc9a
“Rik <cabanier@adobe.com>
2015-03-30 21:26:24 -0700

    added viewbox test

---
+++ viewbox.js
@@ -0,0 +1,192 @@
+{
+	"children" : 
+	[
+		{
+			"children" : 
+			[
+				{
+					"bounds" : 
+					{
+						"bottom" : 448,
+						"left" : 104,
+						"right" : 155,
+						"top" : 399
+					},
+					"id" : "id_1_1",
+					"shape" : 
+					{
+						"path" : "M155.000000,399.000000 L104.000000,399.000000 L104.000000,448.000000 L155.000000,448.000000 z",
+						"type" : "path"
+					},
+					"style" : 
+					{
+						"fill" : 
+						{
+							"color" : 
+							{
+								"a" : 1,
+								"b" : 255,
+								"g" : 255,
+								"r" : 0
+							},
+							"type" : "solid"
+						}
+					},
+					"type" : "shape",
+					"visualBounds" : 
+					{
+						"bottom" : 448,
+						"left" : 104,
+						"right" : 155,
+						"top" : 399
+					}
+				},
+				{
+					"bounds" : 
+					{
+						"bottom" : 419,
+						"left" : 138,
+						"right" : 179,
+						"top" : 373
+					},
+					"id" : "id_1_2",
+					"shape" : 
+					{
+						"path" : "M138.000000,419.000000 L179.000000,419.000000 L179.000000,373.000000 L138.000000,373.000000 z",
+						"type" : "path"
+					},
+					"style" : 
+					{
+						"fill" : 
+						{
+							"color" : 
+							{
+								"a" : 1,
+								"b" : 0,
+								"g" : 255,
+								"r" : 255
+							},
+							"type" : "solid"
+						}
+					},
+					"type" : "shape",
+					"visualBounds" : 
+					{
+						"bottom" : 419,
+						"left" : 138,
+						"right" : 179,
+						"top" : 373
+					}
+				},
+				{
+					"bounds" : 
+					{
+						"bottom" : 560,
+						"left" : 0,
+						"right" : 121,
+						"top" : 468
+					},
+					"id" : "id_1_3",
+					"shape" : 
+					{
+						"path" : "M0.000000,468.000000 L121.000000,468.000000 L121.000000,560.000000 L0.000000,560.000000 z",
+						"type" : "path"
+					},
+					"style" : 
+					{
+						"fill" : 
+						{
+							"color" : 
+							{
+								"a" : 1,
+								"b" : 0,
+								"g" : 0,
+								"r" : 255
+							},
+							"type" : "solid"
+						}
+					},
+					"type" : "shape",
+					"visualBounds" : 
+					{
+						"bottom" : 560,
+						"left" : 0,
+						"right" : 121,
+						"top" : 468
+					}
+				},
+				{
+					"bounds" : 
+					{
+						"bottom" : 92,
+						"left" : 0,
+						"right" : 121,
+						"top" : 0
+					},
+					"id" : "id_1_4",
+					"shape" : 
+					{
+						"path" : "M0.000000,0.000000 L121.000000,0.000000 L121.000000,92.000000 L0.000000,92.000000 z",
+						"type" : "path"
+					},
+					"style" : 
+					{
+						"fill" : 
+						{
+							"color" : 
+							{
+								"a" : 1,
+								"b" : 0,
+								"g" : 255,
+								"r" : 0
+							},
+							"type" : "solid"
+						}
+					},
+					"type" : "shape",
+					"visualBounds" : 
+					{
+						"bottom" : 92,
+						"left" : 0,
+						"right" : 121,
+						"top" : 0
+					}
+				}
+			],
+			"id" : "id_1",
+			"type" : "group",
+			"visualBounds" : 
+			{
+				"bottom" : 560,
+				"left" : 0,
+				"right" : 179,
+				"top" : 0
+			}
+		}
+	],
+	"global" : 
+	{
+		"bounds" : 
+		{
+			"bottom" : 560,
+			"left" : 0,
+			"right" : 960,
+			"top" : 0
+		},
+		"colorMode" : "rgb",
+		"gradients" : {},
+		"masks" : {},
+		"patterns" : {},
+		"pxToInchRatio" : 72,
+		"styles" : {},
+		"viewBox" : 
+		{
+			"bottom" : 560,
+			"left" : 0,
+			"right" : 960,
+			"top" : 0
+		}
+	},
+	"title" : "/Users/cabanier/temp/cabanier.github.io/rect.ai",
+	"version" : "1.0"
+}
